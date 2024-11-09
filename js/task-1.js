bc559bfb47a11b0c75f9c12805c175eac50a607f
Artem Zherdochkin <artem.zherdochkin@gmail.com>
2024-11-10 00:45:55 +0200

    add task-2

--- js/task-1.js
+++ js/task-1.js
@@ -3,9 +3,9 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
   if (totalPrice <= customerCredits) {
-    return `"You ordered ${quantity} droids worth ${totalPrice} credits!"`;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
   } else {
-    return `"Insufficient funds!"`;
+    return `Insufficient funds!`;
   }
 }
 
